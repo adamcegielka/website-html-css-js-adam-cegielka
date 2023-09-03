e9e315a7166f2c8629194b089605470121f910fa
Adam <adamcegielka@proton.me>
2023-09-03 12:05:20 +0200

    feat: new script `owl-carousel`

--- js/script.js
+++ js/script.js
@@ -12,4 +12,21 @@ $(document).ready(() => {
 		startDelay: 1000,
 		showCursor: false
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });
