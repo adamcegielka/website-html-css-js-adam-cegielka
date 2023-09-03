4fb97da8b3140736ed405a6d00143a4b122c670e
Adam <adamcegielka@proton.me>
2023-09-03 14:02:21 +0200

    refactor `script.js` and add activat the charts on scroll

--- js/script.js
+++ js/script.js
@@ -1,46 +1,53 @@
 $(document).ready(() => {
-    $('#slides').superslides({
-        animation: 'fade',
-        play: 5000,
-        pagination: false
-    });
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: false,
+  });
 
-    let typed = new Typed('.typed', {
-        strings: ['Automation Tester', "Manual Tester", 'Paraglider Pilot'],
-		typeSpeed: 70,
-		loop: true,
-		startDelay: 1000,
-		showCursor: false
-    });
+  let typed = new Typed('.typed', {
+    strings: ['Automation Tester', 'Manual Tester', 'Paraglider Pilot'],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
         items: 4,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            938:{
-                items:4
-            }
-        }
-    });
+      },
+    },
+  });
 
-    $('.chart').easyPieChart({
+  let skillsTopOffset = $('.skillSection').offset().top;
+
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $('.chart').easyPieChart({
         easing: 'easeInOut',
         barColor: '#e74c3c',
         trackColor: false,
         scaleColor: false,
         lineWidth: 4,
         size: 152,
-        onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
-        }
-    });
+        onStep: function (from, to, percent) {
+          $(this.el).find('.percent').text(Math.round(percent));
+        },
+      });
+    }
+  });
+  
 });
