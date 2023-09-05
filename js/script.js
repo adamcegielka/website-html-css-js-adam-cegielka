8851e8ca794b1337c6bc41c32b75c5d11d55d25b
Adam <adamcegielka@proton.me>
2023-09-05 16:18:14 +0200

    feat(js): add function for `filters` id

--- js/script.js
+++ js/script.js
@@ -75,6 +75,24 @@ $(document).ready(() => {
     }
   });
 
+  $("#filters a").click(function() {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    const selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+      }
+    });
+
+    return false;
+  });
+
   // $("[data-fancybox]").fancybox();
 
 });
