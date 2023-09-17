a912bc66e35b779d981a2e94d3127b23acba8bcd
Adam <adamcegielka@proton.me>
2023-09-17 12:33:42 +0200

    tests: add more assertions for homepage test

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,7 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('Test homepage', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
-  await expect(page).toHaveTitle(/Adam Cegiełka - My Portfolio/);
+  test('check of the page title and address', async ({ page }) => {
+    await expect(page).toHaveTitle(/Adam Cegiełka - My Portfolio/);
+    await expect(page).toHaveURL('https://adamcegielka.pl/');
+  });
+
+  test('verification of locators', async ({ page }) => {
+    await expect(page.locator('.titleMessage')).toContainText('ADAM CEGIEŁKA');
+    await expect(page.locator('.next')).toBeVisible();
+    await expect(page.locator('.prev')).toBeVisible();
+    await expect(page.locator('#slides')).toBeVisible();
+    await expect(page.locator('.main')).toHaveCount(1);
+    await expect(page.locator('.slides-navigation')).toBeTruthy();
+  });
 });
